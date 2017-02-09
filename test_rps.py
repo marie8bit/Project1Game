8407d11bd225b2f3a5966bfc46d0dfeeeaafc054
marie8bit <yd7581ku@go.minneapolis.edu>
2017-02-09 15:57:25 -0600

    add test for player loses, fixed code so game still works with changes

--- test_rps.py
+++ test_rps.py
@@ -9,7 +9,6 @@ class TestRockPaperScissors(unittest.TestCase):
     @patch('Game.Game.compThrow', side_effect=[3])
     @patch('builtins.input', side_effect=['y', '1', 'n'])
     def test_game_win(self, mock_input, mock_comp_throw):
-
         examplePlayer1 = Player("marie", 10)
         playerList = []
         game = Game(examplePlayer1, playerList)
@@ -19,7 +18,6 @@ class TestRockPaperScissors(unittest.TestCase):
     @patch('Game.Game.compThrow', side_effect=[1])
     @patch('builtins.input', side_effect=['y', '3', 'n'])
     def test_game_lose(self, mock_input, mock_comp_throw):
-
         examplePlayer1 = Player("marie", 10)
         playerList = []
         game = Game(examplePlayer1, playerList)
@@ -28,14 +26,10 @@ class TestRockPaperScissors(unittest.TestCase):
         self.assertEqual(10, examplePlayer1.wins)
 
     def test_game_tie_win(self):
-
         examplePlayer1 = Player("marie", 10)
         computerThrow = 1
-
         game = Game(examplePlayer1, [])
-
         self.assertFalse(game.winning(2 , 2))
-
         # player rock, computer scissors
         self.assertTrue(game.winning(1, 3))
         # test that player gets points
